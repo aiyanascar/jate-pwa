3e49d013b68c7b778bed348fa1858fbdbb49c36e
Aiyana Scarborough <aiyanascar@Aiyanas-Laptop.attlocal.net>
2024-08-11 22:41:59 -0400

    updates

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -7,8 +7,8 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: './client/src/js/index.js',
+      install: './client/src/js/install.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -16,11 +16,11 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
+        template: './client/index.html',
         title: 'J.A.T.E',
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
+        swSrc: './client/src-sw.js',
         swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
@@ -33,7 +33,7 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve('client/src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
